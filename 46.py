77da28b1502cc83bc0ee8a57ff50dfcc9f9984a6
Барышев Владимир Александрович <thevolunteersru@gmail.com>
2023-09-28 15:33:26 +0300

    exercise 46 done byself via stack and dfs

--- 46.py
+++ 46.py
@@ -7,9 +7,29 @@ import itertools
 
 
 def permute(nums: List[int]) -> List[int]:
-	
-	return list(itertools.permutations(nums))
+    
+    # return list(itertools.permutations(nums))
 
+    nums_l = len(nums)
+    result = []
+    stack = [[[], nums]]
+
+    while stack:
+
+        perm, nums = stack.pop()
+
+        if len(perm) == nums_l and perm not in result:
+            result .append(perm)
+
+        for i in range(len(nums)):
+
+            if nums[i] not in perm:
+
+                stack.append([perm+[nums[i]], nums])
+
+            # print(stack)
+
+    return result
 
 
 nums_1 = [1,2,3]
@@ -21,9 +41,9 @@ nums_2 = [0,1]
 nums_3 = [1]
 # Output: [[1]]
 
-print(permute(nums_1))
-print(permute(nums_2))
-print(permute(nums_3))
+print(permute(nums_1), '\n')
+print(permute(nums_2), '\n')
+print(permute(nums_3), '\n')
 
 
 
