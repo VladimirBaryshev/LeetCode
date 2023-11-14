49533bfbc550a7f3075c81351b38cc0095b18e2f
Барышев Владимир Александрович <thevolunteersru@gmail.com>
2023-11-14 15:15:13 +0300

    exercise 70 done by self. But memry limit exceded. Used public solution

---
+++ 70.py
@@ -0,0 +1,66 @@
+# 70. Climbing Stairs
+# https://leetcode.com/problems/climbing-stairs/
+
+
+class Solution:
+
+    # def climbStairs(self, n: int) -> int:
+
+    #     count_combinations = 0
+
+    #     possible_stairs = [1, 2]
+
+    #     stack = [[1, [1]], [2, [2]]]
+
+    #     while stack:
+            
+    #         cur_sum, cur = stack.pop()
+
+    #         if cur_sum < n:
+    #             for s in possible_stairs:
+    #                 stack.append([cur_sum+s, cur[::] + [s]])
+
+    #         elif cur_sum == n:
+    #             print(cur)
+    #             count_combinations += 1
+
+    #     return count_combinations
+    def climbStairs(self, n: int) -> int:
+
+        if n <= 3:
+            return n
+
+        n1, n2 = 2, 3
+
+        for i in range(4, n + 1):
+            temp = n1 + n2
+            n1 = n2
+            n2 = temp
+
+        return n2
+
+        
+
+
+n_1 = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+
+n_2 = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+
+n_3 = 4
+n_4 = 35
+n_5 = 38
+
+# print(Solution().climbStairs(n_1))
+# print(Solution().climbStairs(n_2))
+print(Solution().climbStairs(n_3))
+# print(Solution().climbStairs(n_4))
+# print(Solution().climbStairs(n_5))
