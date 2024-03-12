5a1e717bd6f380cda7c344c67495d785b8232445
Барышев Владимир Александрович <thevolunteersru@gmail.com>
2024-03-12 15:29:42 +0300

    exercise 494 time limit exceeded

---
+++ 494.py
@@ -0,0 +1,52 @@
+# 494. Target Sum
+# https://leetcode.com/problems/target-sum/description/
+
+
+from typing import List
+
+
+def findTargetSumWays(nums: List[int], target: int) -> int:
+        
+    count = 0
+    stack = [[(0, nums[0])], [(0, nums[0]*-1)]]
+
+    while stack:
+
+        # print(stack)
+
+        path = stack.pop()
+        i, cur_total = path[-1]
+        # print(i, cur_total)
+        if i == len(nums)-1 and cur_total == target:
+            count += 1
+
+        if i+1 < len(nums):
+            cur_path = path[::]
+            cur_path.append((i+1, cur_total+nums[i+1]))
+            stack.append(cur_path)
+
+            cur_path = path[::]
+            cur_path.append((i+1, cur_total-nums[i+1]))
+            stack.append(cur_path)
+
+    return count
+
+
+
+
+nums_1 = [1,1,1,1,1]
+target_1 = 3
+# Output: 5
+# Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+# -1 + 1 + 1 + 1 + 1 = 3
+# +1 - 1 + 1 + 1 + 1 = 3
+# +1 + 1 - 1 + 1 + 1 = 3
+# +1 + 1 + 1 - 1 + 1 = 3
+# +1 + 1 + 1 + 1 - 1 = 3
+
+nums_2 = [1]
+target_2 = 1
+# Output: 1
+
+print(findTargetSumWays(nums_1, target_1))
+# print(findTargetSumWays(nums_2, target_2))
