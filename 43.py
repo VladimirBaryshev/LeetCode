8398c55523c344d690d30b8c35d3c9b6170b6664
Барышев Владимир Александрович <thevolunteersru@gmail.com>
2024-03-25 15:48:15 +0300

    exercise 43 done with support of public solution

--- 43.py
+++ 43.py
@@ -3,7 +3,27 @@
 
 def multiply(num1: str, num2: str) -> str:
     
-    return str(int(num1) * int(num2))
+    if "0" == num1 or "0" == num2:
+        return "0"
+
+    res = [0] * (len(num1)+len(num2))
+
+    num1 = num1[::-1]
+    num2 = num2[::-1]
+
+    for i in range(len(num1)):
+        for j in range(len(num2)):
+            digit = int(num1[i]) * int(num2[j])
+            res[i+j] += digit
+            res[i+j+1] += res[i+j]//10
+            res[i+j] = res[i+j]%10
+
+    while res[-1] == 0:
+        res.pop()
+
+    return "".join(map(str, res[::-1]))
+
+
 
 
 num1_1 = "2"
