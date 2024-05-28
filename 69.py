f89e0f5c01cf727b3cb83bbf9889b6c51fb651b5
Барышев Владимир Александрович <thevolunteersru@gmail.com>
2024-05-28 13:57:51 +0300

    exercise 69 done with support of public solution

---
+++ 69.py
@@ -0,0 +1,37 @@
+# 69. Sqrt(x)
+# https://leetcode.com/problems/sqrtx/description/
+
+
+class Solution:
+
+    def mySqrt(self, x: int) -> int:
+    	
+    	low = 0
+    	high = x
+    	ans = 1
+
+    	while low <= high:
+    		mid = (low+high)//2
+    		print(mid, ans, low, high)
+    		if mid*mid <= x:
+    			ans = mid
+    			low = mid+1
+    		else:
+    			high = mid-1
+
+    	return ans
+
+
+
+x_1 = 4
+# Output: 2
+# Explanation: The square root of 4 is 2, so we return 2.
+
+x_2 = 8
+# Output: 2
+# Explanation: The square root of 8 is 2.82842..., 
+# and since we round it down to the nearest integer, 2 is returned.
+
+
+print(Solution().mySqrt(x_1))
+print(Solution().mySqrt(x_2))
