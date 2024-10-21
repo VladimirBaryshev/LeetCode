d13529511f8fbf5cae183889b6971bbfd9000ccf
Барышев Владимир Александрович <thevolunteersru@gmail.com>
2024-10-21 15:03:05 +0300

    exercise 438 done byself

---
+++ 438.py
@@ -0,0 +1,46 @@
+# 438. Find All Anagrams in a String
+
+from typing import List
+from collections import Counter
+
+class Solution:
+
+    def countSubstring(self, substr: str) -> str:
+        
+        counted = dict()
+        for i in substr:
+            counted[i] = counted.get(i,0)
+            counted[i] += 1
+        return counted
+
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+
+        L = len(p)
+        # counted = self.countSubstring(p)
+        counted = Counter(p)
+
+        result = []
+        for i,v in enumerate(s):
+            if v in counted.keys() and Counter(s[i:i+L]) == counted:
+                result.append(i)
+        return result
+
+
+
+s_1 = "cbaebabacd"
+p_1 = "abc"
+# Output: [0,6]
+# Explanation:
+# The substring with start index = 0 is "cba", which is an anagram of "abc".
+# The substring with start index = 6 is "bac", which is an anagram of "abc".
+
+s_2 = "abab"
+p_2 = "ab"
+# Output: [0,1,2]
+# Explanation:
+# The substring with start index = 0 is "ab", which is an anagram of "ab".
+# The substring with start index = 1 is "ba", which is an anagram of "ab".
+# The substring with start index = 2 is "ab", which is an anagram
+
+print(Solution().findAnagrams(s_1, p_1))
+print(Solution().findAnagrams(s_2, p_2))
