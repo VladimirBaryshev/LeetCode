abc4f7fda5e7e68927308a4c2fb8662ceafedb6f
Барышев Владимир Александрович <thevolunteersru@gmail.com>
2024-11-27 17:04:03 +0300

    exercise 525 done by self with revision on public solution

--- 525.py
+++ 525.py
@@ -1,28 +1,49 @@
 # 525. Contiguous Array
 
+from collections import defaultdict
 from typing import List
 
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         
-        hashmap = {0:-1}
+        # hashmap = {0:-1}
+        # count = 0
+        # answer = 0
+
+        # for i,v in enumerate(nums):
+
+        #     if v == 0:
+        #         count -= 1
+        #     else:
+        #         count += 1
+
+        #     if count in hashmap.keys():
+        #         answer = max(answer, i - hashmap[count])
+        #     else:
+        #         hashmap[count] = i
+
+        # return answer
+
+        hashmap = defaultdict(list)
         count = 0
-        answer = 0
 
         for i,v in enumerate(nums):
+            hashmap[count].append(i)
 
+            if v == 1:
+                count += 1
             if v == 0:
                 count -= 1
-            else:
-                count += 1
 
-            if count in hashmap.keys():
-                answer = max(answer, i - hashmap[count])
-            else:
-                hashmap[count] = i
+        hashmap[count].append(i+1)
 
-        return answer
+        max_ = 0
+        for k,v in hashmap.items():
+            if len(v) > 1:
+                print(k, v, nums, nums[v[0]:v[-1]])
+                max_ = max(len(nums[v[0]:v[-1]]), max_)
 
+        return max_
 
 
 
@@ -42,8 +63,8 @@ nums_3 = [0,0,1,0,0,0,1,1]
 nums_4 = [0, 1, 1, 0, 1, 1, 1, 0]
 # Output: 4
 
-# print(Solution().findMaxLength(nums_1))
-# print(Solution().findMaxLength(nums_2))
-# print(Solution().findMaxLength(nums_3))
+print(Solution().findMaxLength(nums_1))
+print(Solution().findMaxLength(nums_2))
+print(Solution().findMaxLength(nums_3))
 print(Solution().findMaxLength(nums_4))
 
